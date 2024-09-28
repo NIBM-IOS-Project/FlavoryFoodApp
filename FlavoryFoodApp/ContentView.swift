3b14578f41ae339c672b49d9e988382bc8c3fdc4
Tharusha <tharusha.astro@gmail.com>
2024-09-28 09:57:18 +0530

    Initial Commit

---
+++ FlavoryFoodApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlavoryFoodApp
+//
+//  Created by COCOBSCCOMP23.1P-022 on 2024-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlavoryFoodApp/FlavoryFoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavoryFoodAppApp.swift
+//  FlavoryFoodApp
+//
+//  Created by COCOBSCCOMP23.1P-022 on 2024-09-28.
+//
+
+import SwiftUI
+
+@main
+struct FlavoryFoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
