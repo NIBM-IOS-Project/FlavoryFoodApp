bd71c0c1c563d94b1fdd1ba9bc4c4dcb16ac5472
Tharusha <tharusha.astro@gmail.com>
2024-10-05 08:38:00 +0530

    push to tharusha

--- FlavoryFoodApp/ContentView.swift
+++ FlavoryFoodApp/ContentView.swift
@@ -10,15 +10,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Project Updated!")
+        ZStack {
+            // Background Image
+            Image("MainScreen.png")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color.orange)
+                .padding()
+                .edgesIgnoringSafeArea(.all)
+
+            // Overlay Text and Button
+            VStack {
+                Spacer()
+                
+                // App Name
+                Text("Flavorly")
+                    .font(.system(size: 48, weight: .bold))
+                    .foregroundColor(.orange)
+                    .padding(.bottom, 20)
+                
+                // Start Cooking Button
+                Button(action: {
+                    // Add button action here
+                }) {
+                    HStack {
+                        Text("Start Cooking")
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                        Image(systemName: "arrow.right")
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding(.horizontal, 20)
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
 
 
